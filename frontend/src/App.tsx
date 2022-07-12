89b9575d33792bfde54737f1c97acfc2d7a1f316
Joyce Fernandes <joyce_f.s@hotmail.com>
2022-07-12 16:14:20 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotioficationButton"
+
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
